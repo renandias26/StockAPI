f64f401c429302f0678e2e1572580e7ff097a798
Renan Dias <renanrsd26.rd@gmail.com>
2024-04-08 19:36:34 -0300

    Refactor
    Add gitignore and readme - Change architecture project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Stock API
+
+This API have following CRUD's
+- Products
+- Stock
+- User
+
+> It was maked with TS, Express and Mongoose
--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,5 +1,5 @@
 import Product from "./Product";
-import { ProductType} from "./Types/ProductTypes";
+import { ProductType } from "./Types/ProductTypes";
 import fs from 'fs'
 
 class ProductService {
@@ -23,51 +23,51 @@ class ProductService {
     }
 
     async update(id, dataToUpdate: ProductType) {
-        const updatedProduct = await Product.findOneAndUpdate({_id: id}, {
+        const updatedProduct = await Product.findOneAndUpdate({ _id: id }, {
             nome: dataToUpdate.nome,
             qtde: dataToUpdate.qtde,
             preco: dataToUpdate.preco
 
-        }, {new: true})
+        }, { new: true })
 
         return updatedProduct
     }
 
     async delete(id) {
-        await Product.findOneAndDelete({_id: id})
+        await Product.findOneAndDelete({ _id: id })
         return
     }
-    async random(lista) {  
+    async random(lista) {
 
 
         const randomList: Array<ProductType> = []
 
         while (randomList.length < 4) {
-            const rand: Number = Math.floor(Math.random() * lista.length)
-            if(!randomList.includes(lista[rand])) {
-              randomList.push(lista[rand])
+            const rand = Math.floor(Math.random() * lista.length)
+            if (!randomList.includes(lista[rand])) {
+                randomList.push(lista[rand])
             }
-          
-          }
 
-          console.log(randomList)
-        
-          return randomList
+        }
+
+        console.log(randomList)
+
+        return randomList
 
     }
 
 
     async writefile() {
-        
+
         const lista = await this.list()
 
         const data = JSON.stringify(lista)
 
         fs.writeFile('products.json', data, (err) => {
-            if (err){
+            if (err) {
                 throw err;
             }
-            else{
+            else {
                 console.log("Dados gravados com sucesso")
             }
         })
@@ -75,18 +75,18 @@ class ProductService {
 
     async readProductsFile() {
 
-    
+
         try {
-            const data = fs.readFileSync('products.json')
+            const data = fs.readFileSync('products.json', { encoding: "utf-8" })
             console.log("Lendo dados!")
             return JSON.parse(data)
 
         } catch (err) {
             console.log("Erro na hora de ler produtos:", err)
-        
+
         }
 
-        
+
 
     }
 
